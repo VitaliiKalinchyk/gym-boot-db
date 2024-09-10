4a913da0ac0ca6088ae0917cd55010f373f93011
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-10 18:21:14 +0300

    initial commit

---
+++ src/main/java/epam/task/gymbootdb/GymBootDbApplication.java
@@ -0,0 +1,12 @@
+package epam.task.gymbootdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBootDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBootDbApplication.class, args);
+    }
+}
---
+++ src/test/java/epam/task/gymbootdb/GymBootDbApplicationTests.java
@@ -0,0 +1,12 @@
+package epam.task.gymbootdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBootDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
