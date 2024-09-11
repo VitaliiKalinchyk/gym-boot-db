03ab7f1ca9fbdce0b50244c847b8748c020f9066
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-11 13:54:38 +0300

    Added base functionality

--- src/main/java/epam/task/gymbootdb/repository/TrainingRepository.java
+++ src/main/java/epam/task/gymbootdb/repository/TrainingRepository.java
@@ -3,5 +3,8 @@ package epam.task.gymbootdb.repository;
 import epam.task.gymbootdb.entity.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrainingRepository extends JpaRepository<Training, Long> {
+    Optional<Training> findByName(String name);
 }
