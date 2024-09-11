2d66475e2da2f9078817a44be26eb5799643a91a
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-11 13:48:13 +0300

    Added entities

---
+++ src/main/java/epam/task/gymbootdb/entity/Training.java
@@ -0,0 +1,40 @@
+package epam.task.gymbootdb.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "training")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Training {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private LocalDate date;
+
+    private int durationMinutes;
+
+    @ManyToOne
+    @JoinColumn(name = "trainer_id")
+    private Trainer trainer;
+
+    @ManyToOne
+    @JoinColumn(name = "trainee_id")
+    private Trainee trainee;
+
+    @ManyToOne
+    @JoinColumn(name = "training_type_id")
+    private TrainingType trainingType;
+}
---
+++ src/main/java/epam/task/gymbootdb/entity/User.java
@@ -0,0 +1,27 @@
+package epam.task.gymbootdb.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private boolean isActive;
+}
