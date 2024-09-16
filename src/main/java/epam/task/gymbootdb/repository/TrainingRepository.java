f6034e2717798d049f80849c5e2472ecb619a75e
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-16 13:18:45 +0300

    Removed unnecessary @Param annotations for repositories

--- src/main/java/epam/task/gymbootdb/repository/TrainingRepository.java
+++ src/main/java/epam/task/gymbootdb/repository/TrainingRepository.java
@@ -4,7 +4,6 @@ import epam.task.gymbootdb.entity.Training;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,19 +16,19 @@ public interface TrainingRepository extends JpaRepository<Training, Long> {
             "(:trainerUsername IS NULL OR t.trainer.user.username = :trainerUsername) AND " +
             "(:trainingTypeName IS NULL OR t.trainingType.name = :trainingTypeName)")
     List<Training> findTraineeTrainingsByOptionalParams(
-            @Param("traineeUsername") String traineeUsername,
-            @Param("fromDate") LocalDate fromDate,
-            @Param("toDate") LocalDate toDate,
-            @Param("trainerUsername") String trainerUsername,
-            @Param("trainingTypeName") String trainingTypeName);
+            String traineeUsername,
+            LocalDate fromDate,
+            LocalDate toDate,
+            String trainerUsername,
+            String trainingTypeName);
 
     @Query("SELECT t FROM Training t WHERE t.trainer.user.username = :trainerUsername AND " +
             "(:fromDate IS NULL OR t.date >= :fromDate) AND " +
             "(:toDate IS NULL OR t.date <= :toDate) AND " +
             "(:traineeUsername IS NULL OR t.trainee.user.username = :traineeUsername)")
     List<Training> findTrainerTrainingsByOptionalParams(
-            @Param("trainerUsername") String trainerUsername,
-            @Param("fromDate") LocalDate fromDate,
-            @Param("toDate") LocalDate toDate,
-            @Param("traineeUsername") String traineeUsername);
+            String trainerUsername,
+            LocalDate fromDate,
+            LocalDate toDate,
+            String traineeUsername);
 }
